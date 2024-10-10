a4572017a21eccf4b9ac9cbeab7cc8b47ea37d15
Thiago Oliveira <129562958+ThiagoOliveiraJ@users.noreply.github.com>
2024-10-10 14:01:41 -0300

    feat: add home and header

---
+++ Equilibrium/src/Pages/Home/index.jsx
@@ -0,0 +1,17 @@
+import imgHome from '../../assets/mindfulness-animate.svg'
+import './home.modules.css'
+function Home({ user }) {
+    
+    return (
+        <main className="section-screen" id="home">
+            <article className="left">
+                <h1 className="reveal">Bem-vindo(a) ao  Equilibrium, <span>{user}!</span> Seu espaço para encontrar o equilíbrio que transforma.</h1>
+            </article>
+            <div className="right">
+                <img className='img-principal' src={imgHome} />
+            </div>
+        </main>
+    )
+}
+
+export default Home;
---
+++ Equilibrium/src/components/header/index.jsx
@@ -0,0 +1,16 @@
+import './header.modules.css'
+function Header(){
+    return(
+        <header>
+            <h3 className="logo">Equilibrium</h3>
+            <ul className="menu">
+                <li>Início</li>
+                <li>Sobre</li>
+                <li>Livros</li>
+                <li></li>
+            </ul>
+        </header>
+    );
+}
+
+export default Header;
