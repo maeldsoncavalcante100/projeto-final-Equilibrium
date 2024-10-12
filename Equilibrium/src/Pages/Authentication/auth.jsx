f3762481b1d275c5dc78adac10033976e48e215b
Adler Sebastian <adlersebas91@gmail.com>
2024-10-12 19:48:20 -0300

    feat: new functions in auth

--- Equilibrium/src/Pages/Authentication/auth.jsx
+++ Equilibrium/src/Pages/Authentication/auth.jsx
@@ -18,4 +18,57 @@ export const AuthProvider = ({ children }) => {
     }
   }, []);
 
-  
+  const signin = (email, password) => {
+    const usersStorage = JSON.parse(localStorage.getItem("users_bd"));
+
+    const hasUser = usersStorage?.filter((user) => user.email === email);
+
+    if (hasUser?.length) {
+      if (hasUser[0].email === email && hasUser[0].password === password) {
+        const token = Math.random().toString(36).substring(2);
+        localStorage.setItem("user_token", JSON.stringify({ email, token }));
+        setUser({ email, password });
+        return;
+      } else {
+        return "E-mail ou senha incorretos";
+      }
+    } else {
+      return "Usuário não registrado";
+    }
+  };
+
+  const signup = (email, password) => {
+    const usersStorage = JSON.parse(localStorage.getItem("users_bd"));
+
+    const hasUser = usersStorage?.filter((user) => user.email === email);
+
+    if (hasUser?.length) {
+      return "Esse email já possui conta";
+    }
+
+    let newUser;
+
+    if (usersStorage) {
+      newUser = [...usersStorage, { email, password }];
+    } else {
+      newUser = [{ email, password }];
+    }
+
+    localStorage.setItem("users_bd", JSON.stringify(newUser));
+
+    return;
+  };
+
+  const signout = () => {
+    setUser(null);
+    localStorage.removeItem("user_token");
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ user, signed: !!user, signin, signup, signout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
